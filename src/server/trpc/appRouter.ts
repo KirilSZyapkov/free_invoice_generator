350bef158c3ae360fcb1b90e981984f88d9c5cd0
Kiril Zyapkov <appdeveloperjs47@gmail.com>
2025-11-03 10:33:17 +0200

    feat: adding the emailRouter to appRouter

--- src/server/trpc/appRouter.ts
+++ src/server/trpc/appRouter.ts
@@ -1,10 +1,12 @@
 import {router} from "./router";
-import { invoiceRouter } from "./routers/invoice";
+import {invoiceRouter} from "./routers/invoice";
 import {userRouter} from "./routers/user";
+import {emailRouter} from "./routers/email";
 
 export const appRouter = router({
   invoice: invoiceRouter,
-  user: userRouter
+  user: userRouter,
+  email: emailRouter
 });
 
 export type AppRouter = typeof appRouter;
--- src/server/trpc/routers/email.ts
+++ src/server/trpc/routers/email.ts
@@ -1,12 +1,19 @@
-import { router, publicProcedure } from '../router';
-import { sendInvoiceEmail} from "../../utils/mail";
+import {router, publicProcedure} from '../router';
+import {sendInvoiceEmail} from "../../utils/mail";
 
 export const emailRouter = router({
-  sendImails: publicProcedure.query(async ({ctx, input})=>{
-    const to = input.to;
-    const pdfBuffer = input.pdfBuffer;
-    const invoiceNumber = input.invoiceNumber;
+  sendImails: publicProcedure.query(async ({ctx, input}) => {
+    const to: string = input.to;
+    const pdfBuffer: Buffer[] = input.pdfBuffer;
+    const invoiceNumber: string = input.invoiceNumber;
 
+    try {
+      await sendInvoiceEmail(to, pdfBuffer, invoiceNumber)
+      return {uccess: true}
+    } catch (e) {
+      console.error("❌ Email error:", e);
+      throw new Error("Failed to send the email");
+    }
 
   })
 })
