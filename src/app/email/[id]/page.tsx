5a5a81b739f6edbcf6be8b51f50c1ed719005d4f
Kiril Zyapkov <appdeveloperjs47@gmail.com>
2025-11-21 07:42:33 +0200

    fix: update localStorage handling to include timestamp and improve data retrieval in useLocalStorage hook

--- src/app/email/[id]/page.tsx
+++ src/app/email/[id]/page.tsx
@@ -18,7 +18,7 @@ const EmailPage = () => {
     async function buffer() {
       if (id === "invoice") {
         console.log(data[0]);
-        const localInvoice = data[0];
+        const localInvoice = data[0].value;
         if (!localInvoice) {
           setError("No invoice data in local storage");
           return;
--- src/utils/hooks/useLocalStorage.tsx
+++ src/utils/hooks/useLocalStorage.tsx
@@ -3,18 +3,41 @@
 import { useState, useEffect } from "react";
 
 export function useLocalStorage<T>(key: string, initialValue: T | (() => T)) {
+  const expirationMs = 30 * 24 * 60 * 60 * 1000; // 30 days
+
   const [data, setData] = useState<T>(() => {
-    const jsonValue = localStorage.getItem(key);
-    if (jsonValue !== null) return JSON.parse(jsonValue);
-    if (typeof initialValue === "function") {
-      return (initialValue as () => T)();
-    } else {
-      return initialValue;
+    try {
+      const jsonValue = localStorage.getItem(key);
+      if (jsonValue !== null) {
+        const parsed = JSON.parse(jsonValue);
+
+        if (parsed.timestamp && (Date.now() - parsed.timestamp) > expirationMs) {
+          localStorage.removeItem(key);
+          return typeof initialValue === "function"
+            ? (initialValue as () => T)()
+            : initialValue;
+        }
+        return parsed.value;
+      }
+
+      if (typeof initialValue === "function") {
+        return (initialValue as () => T)();
+      } else {
+        return initialValue;
+      }
+    } catch {
+      return typeof initialValue === "function"
+        ? (initialValue as () => T)()
+        : initialValue;
     }
   });
 
   useEffect(() => {
-    localStorage.setItem(key, JSON.stringify(data));
+    const wrapper={
+      value: data,
+      timestamp: Date.now()
+    }
+    localStorage.setItem(key, JSON.stringify(wrapper));
   }, [data, setData]);
 
   return [data, setData] as [typeof data, typeof setData];
